96cf96f992f772b59b4b1fd2dc5795527288e458
eduardostern <92046555+eduardostern@users.noreply.github.com>
2022-12-10 17:23:41 -0300

    Update app.vue
    Turnstile

--- app.vue
+++ app.vue
@@ -4,7 +4,8 @@
   </div>
 </template>
 
+<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+
 <style>
 @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
-
 </style>
