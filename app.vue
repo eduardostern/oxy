648ea77da340cdea407f37e995cf2230e9b73ea2
eduardostern <92046555+eduardostern@users.noreply.github.com>
2022-12-10 17:27:11 -0300

    Update app.vue
    Back fix

--- app.vue
+++ app.vue
@@ -4,8 +4,6 @@
   </div>
 </template>
 
-<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
-
 <style>
 @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
 </style>
